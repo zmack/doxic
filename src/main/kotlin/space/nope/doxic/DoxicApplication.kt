b2f9fba455c53a2f1d1514f45d34b9fc9342938c
Andrei Bocan <zmaxor@gmail.com>
2021-06-28 23:11:13 -0700

    Tabs are for nerds

--- src/main/kotlin/space/nope/doxic/DoxicApplication.kt
+++ src/main/kotlin/space/nope/doxic/DoxicApplication.kt
@@ -10,46 +10,46 @@ import javax.script.ScriptEngineManager
 import javax.script.SimpleBindings
 
 class Kilt {
-	fun dance() = println("I'm dancing in my kilt")
+    fun dance() = println("I'm dancing in my kilt")
 }
 
 @SpringBootApplication
 class DoxicApplication: CommandLineRunner {
-	override fun run(vararg args: String?) {
+    override fun run(vararg args: String?) {
         val parser = Parser.builder().build()
-		val engine = ScriptEngineManager().getEngineByExtension("kts")
+        val engine = ScriptEngineManager().getEngineByExtension("kts")
 
-		val markdown = """
-			Hello you guys
-			```gunner
-			println("I am the script")
-			```
-		""".trimIndent()
+        val markdown = """
+            Hello you guys
+            ```gunner
+            println("I am the script")
+            ```
+        """.trimIndent()
 
-		val simpleBindings = SimpleBindings(mapOf("kilt" to Kilt()))
-		engine.setBindings(simpleBindings, ScriptContext.GLOBAL_SCOPE)
+        val simpleBindings = SimpleBindings(mapOf("kilt" to Kilt()))
+        engine.setBindings(simpleBindings, ScriptContext.GLOBAL_SCOPE)
 
-		val document = parser.parse(markdown)
-		document.children.forEach {
-			when(it) {
-				is FencedCodeBlock -> {
-					println("Fenced block ${it.info}")
-					println(engine.eval(it.contentChars.toString()))
-				}
-			}
-		}
+        val document = parser.parse(markdown)
+        document.children.forEach {
+            when(it) {
+                is FencedCodeBlock -> {
+                    println("Fenced block ${it.info}")
+                    println(engine.eval(it.contentChars.toString()))
+                }
+            }
+        }
 
-		val script = """
-			println("Hello from this inner script")
-			println(kilt)
-			kilt
-			""".trimIndent()
+        val script = """
+            println("Hello from this inner script")
+            println(kilt)
+            kilt
+            """.trimIndent()
 
-		println(engine.eval(script))
+        println(engine.eval(script))
 
-	}
+    }
 }
 
 fun main(args: Array<String>) {
-	runApplication<DoxicApplication>(*args)
+    runApplication<DoxicApplication>(*args)
 }
